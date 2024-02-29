be0a8b6ab2a95dc45d45ebc28eb6eb81860a05e7
Yashpreet Singh <singhyashpreet22@gmail.com>
2024-02-29 23:39:08 +0530

    Added Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cM: "Chillax-Medium",
+        cR: "Chillax-Regular",
+        cSB: "Chillax-SemiBold",
+        cuda: "CUDA",
+        samarkan: "Samarkan",
+      },
+    },
   },
   plugins: [],
 }
