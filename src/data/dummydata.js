1ee69273400b4d5e4949310a93f7eb050b1839e3
Nitesh Singh Bisht <niteshsinghbisht003@gmail.com>
2024-03-03 02:34:18 -0800

    Event Page Completed

--- src/data/dummydata.js
+++ src/data/dummydata.js
@@ -1,7 +1,12 @@
+import img1 from "../assets/Images/Dramaflict_planet.webp"
+import img2 from "../assets/Images/Fusion_Planet.webp"
+import img3 from "../assets/Images/Flashmob_Planet.webp"
+
 export const data = [
     {
     id:1,
     name:"DRAMAFLICT",
+    img:img1,
     Event:[
     {
     id:1,
@@ -17,7 +22,7 @@ export const data = [
 {
     id:2,
     name:"FUSION FEVER",
-    
+    img:img2,
     Event:[
         {
         id:1,
@@ -33,6 +38,7 @@ export const data = [
 {
     id:2,
     name:"FUSION FEVER",
+    img:img3,
     Event:[
         {
         id:1,
@@ -48,6 +54,7 @@ export const data = [
 {
     id:2,
     name:"FUSION FEVER",
+    img:img1,
     Event:[
         {
         id:1,
@@ -63,6 +70,7 @@ export const data = [
 {
     id:2,
     name:"FUSION FEVER",
+    img:img2,
     Event:[
         {
         id:1,
@@ -72,12 +80,23 @@ export const data = [
         name:"minmi Act"},
         {
         id:3,
-        name:"minmi Act"}
+        name:"minmi Act"}, 
+        {
+            id:4,
+            name:"minmi Act"},
+            {
+            id:5,
+            name:"minmi Act"},
+            {
+            id:6,
+            name:"minmi Act"}
+
     ]
 },
 {
     id:2,
     name:"FUSION FEVER",
+    img:img3,
     Event:[
         {
         id:1,
