b5f28be43df914e98316f472e03c14b97cac8c8c
Yashpreet Singh <singhyashpreet22@gmail.com>
2024-03-04 22:10:48 +0530

    Added Routes to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,38 @@
-import homePageVid from "../assets/Videos/HeroBG.webm";
-
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-        <div className="flex justify-between items-center py-10 px-10 font-cM w-[100%] bg-transparent absolute">    
-            
+        <div className="flex justify-between items-center py-10 px-10 font-cM w-full bg-transparent absolute z-20">    
                 <div className="font-cM text-xl">
-                    <h1 className="text-white font-bold">GOONJ '<span className='text-[#5f43b2] font-bold'>24</span></h1>
+                    <h1 className="text-white">GOONJ<span className='text-[#5f43b2]'>'24</span></h1>
                 </div>
                 <div className="flex">
                     <ul className="flex justify-between text-center gap-6 text-white text-xl">
                         <li>
-                            <a href="#home" className="nav-link ease-in duration-200 tracking-wider transition-colors hover:underline">HOME</a>
+                            <Link to="/">HOME</Link>
                         </li>
                         <li>
-                            <a href="#about Us" className="nav-link text ease-in duration-200  tracking-wider hover:underline">EVENTS</a>
+                            <Link to="/events">EVENTS</Link> 
                         </li>
                         <li>
-                            <a href="#Menu" className="nav-link ease-in duration-200  tracking-wider  hover:underline">GALLERY</a>
+                            <Link to="/gallery">GALLERY</Link>
                         </li>
                         <li>
-                            <a href="#Review" className="nav-link hover:text-decoration-underline ease-in duration-200  tracking-wider hover:underline">SPONSORS</a>
+                            <Link to="/sponsors">SPONSORS</Link>
                         </li>
                         <li>
-                            <a href="#Contact" className="nav-link hover:text-decoration ease-in duration-200  tracking-wider hover:underline">TEAMS</a>
+                            <Link to="/teams">TEAMS</Link>
                         </li>
                         <li>
-                            <a href="#Contact" className="nav-link hover:text-decoration ease-in duration-200  tracking-wider hover:underline">CA PORTAL</a>
+                            <Link to="/caportal">CA PORTAL</Link>
                         </li>
-                        <div className="top-1">
-                        <button type="button" className="bg-[#7a59df] rounded-3xl w-24 px-1 hover:bg-[#5c2ee3] ">
-                            LOGIN
-
-                        </button>
-
+                        <div className="">
+                            <button type="button" className="bg-[#7a59df] rounded-3xl w-24 px-1 hover:bg-[#5c2ee3] ">
+                                <Link to="/login">LOGIN</Link>
+                            </button>
                         </div>
-
                     </ul>
                 </div>
-
         </div>
 
     );
