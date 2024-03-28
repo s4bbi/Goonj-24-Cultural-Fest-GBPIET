49180d1a610e1eb20b93ba3d29cf6aa7bb9c9c0b
Yashpreet Singh <singhyashpreet22@gmail.com>
2024-03-28 17:25:34 +0530

    Corrected Event Details Pages

--- frontend/src/Pages/EventDetail.jsx
+++ frontend/src/Pages/EventDetail.jsx
@@ -11,26 +11,24 @@ const EventDetail = () => {
 
     return (
         <div className="w-full bg-cover">
-            <div className="bg-bg_image bg-cover py-16 px-5 md:px-20 md:py-36 h-full hover:none">
+            <div className="bg-bg_image bg-cover py-20 px-5 md:px-20 md:py-36 h-full hover:none">
                 <div className="text-white">
-                    <div className="w-full flex justify-between py-4 items-center">
+                    <div className="w-full flex justify-between items-center">
                         <div className="font-cuda md:text-4xl">
                             {eventDetail.name}
                         </div>
-                        <div className="flex gap-5 text-sm">
-                            <button className="bg-[#5F43B2] font-cM md:text-xl px-6 py-1 rounded-full shadow-md shadow-[#5F43B2] hover:bg-[#5c2ee3]">
+                        <div className="flex gap-2 sm:gap-5 text-sm">
+                            <button className="bg-[#5F43B2] font-cM md:text-xl px-4 py-1 rounded-full shadow-md shadow-[#5F43B2] hover:bg-[#5c2ee3]">
                                 REGISTER
                             </button>
                             <Link to="">
-                                <button className="w-full bg-[#5F43B2] font-cM md:text-xl px-6 py-1 rounded-full shadow-md shadow-[#5F43B2] hover:bg-[#5c2ee3]">
+                                <button className="w-full bg-[#5F43B2] font-cM md:text-xl px-4 py-1 rounded-full shadow-md shadow-[#5F43B2] hover:bg-[#5c2ee3]">
                                     RULE BOOK
                                 </button>
                             </Link>    
                         </div>
                     </div>
                     <div>
-                        
-                        
                         <div className="text-white leading-7">
                             <h1 className="text-sm md:text-2xl font-cSB my-5">GUIDELINES:</h1>
                             <ul className="list-disc ml-8 md:ml-16 font-cR text-justify mb-8">
@@ -41,7 +39,7 @@ const EventDetail = () => {
                         </div>
                         <div className="font-normal font-cR md:text-xl text-sm text-white md:my-10">
                             <p><span className="font-cM">JUDGING CRITERIA: </span>{eventDetail.judging}</p>
-                            <p><span className="font-cR md:my-10">VOTING CRITERIA: </span>60% judges votes + 20% jury votes + 20% audience votes will determine the final winner.</p>
+                            <p><span className="font-cM md:my-10">VOTING CRITERIA: </span>60% judges votes + 20% jury votes + 20% audience votes will determine the final winner.</p>
                         </div>
                     </div>
                 </div>
