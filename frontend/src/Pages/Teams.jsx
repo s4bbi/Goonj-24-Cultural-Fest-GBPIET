bd6104635265e63fe39ae1afedf9cb4908aa0a08
Nitesh Singh Bisht <niteshsinghbisht003@gmail.com>
2024-04-15 14:45:16 +0530

    designation added

--- frontend/src/Pages/Teams.jsx
+++ frontend/src/Pages/Teams.jsx
@@ -1,14 +1,14 @@
 import TeamsVid from "../assets/Videos/TeamsVid.webm";
-import Card from "../components/Card"
-import {teamData} from "../data/TeamData"
+import Card from "../components/Card";
+import { teamData } from "../data/TeamData";
 const Teams = () => {
-    return (
-       <div className="bg-EventBG">
-        <div className="relative flex items-center justify-center h-screen">
-            <div className="text-white z-30 md:text-[150px] font-cuda text-5xl ">
-                TEAMS
-            </div>
-            <video
+  return (
+    <div className="bg-EventBG">
+      <div className="relative flex items-center justify-center h-screen">
+        <div className="text-white z-30 md:text-[150px] font-cuda text-5xl ">
+          TEAMS
+        </div>
+        <video
           autoPlay
           loop
           muted
@@ -17,24 +17,25 @@ const Teams = () => {
           <source src={TeamsVid} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-        </div>
-          <div className="w-10/12 mx-auto">
-            {teamData.map((item)=>(
-              <div key={item.id}>
-                <p  className="text-white font-cuda text-bold text-3xl md:text-[50px] py-20 text-center">{item.name}</p>
-                <div className="mx-auto w-full sm:flex sm:justify-evenly  sm:gap-20 sm:flex-wrap font-cuda">
-                  {item.members.map((member)=>( 
-                    <div key={member.id}>
-                    <Card data={member} />
-                    </div>
-                  ))}
-              </div>
-              </div>
-            ))}
-          </div>      
-       </div>
-        
-    )
-}
+      </div>
+      <div className="w-10/12 mx-auto">
+        {teamData.map((item) => (
+          <div key={item.id}>
+            <p className="text-white font-cuda text-bold text-3xl md:text-[50px] py-20 text-center">
+              {item.name}
+            </p>
+            <div className="mx-auto w-full sm:flex sm:justify-evenly  sm:gap-20 sm:flex-wrap font-cuda">
+              {item.members.map((member) => (
+                <div key={member.id}>
+                  <Card data={member} />
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Teams
+export default Teams;
--- frontend/src/data/TeamData.js
+++ frontend/src/data/TeamData.js
@@ -1,116 +1,124 @@
-import yashi from "../assets/Images/TeamsImages/DevTeam/Yashpreet.png"
-import Nitesh from "../assets/Images/TeamsImages/DevTeam/Nitesh.png"
-import vaibhav from "../assets/Images/TeamsImages/DevTeam/vaibhav.jpg"
-import rohit from "../assets/Images/TeamsImages/DevTeam/rohit.jpeg"
-import priya from "../assets/Images/TeamsImages/DevTeam/priya.jpg"
-import ankur from "../assets/Images/TeamsImages/DevTeam/ankur.jpg"
-import chandu from "../assets/Images/TeamsImages/CoreTeam/chandu.jpeg"
-import niharika from "../assets/Images/TeamsImages/GraphicsTeam/niharika.jpeg"
-import aditya from "../assets/Images/TeamsImages/GraphicsTeam/aditya.jpg"
+import yashi from "../assets/Images/TeamsImages/DevTeam/Yashpreet.png";
+import Nitesh from "../assets/Images/TeamsImages/DevTeam/Nitesh.png";
+import vaibhav from "../assets/Images/TeamsImages/DevTeam/vaibhav.jpg";
+import rohit from "../assets/Images/TeamsImages/DevTeam/rohit.jpeg";
+import priya from "../assets/Images/TeamsImages/DevTeam/priya.jpg";
+import ankur from "../assets/Images/TeamsImages/DevTeam/ankur.jpg";
+import chandu from "../assets/Images/TeamsImages/CoreTeam/chandu.jpeg";
+import niharika from "../assets/Images/TeamsImages/GraphicsTeam/niharika.jpeg";
+import aditya from "../assets/Images/TeamsImages/GraphicsTeam/aditya.jpg";
 // jb sbki ajaygi tb bulk convert krdunga webp mein
 
 export const teamData = [
-    {
-        id:1,
-        name:"Core Team",
-        members:[{
-            id:101,
-            name:"Chandra Prakash",
-            img:chandu,
-            git:"",
-            linkdin:"https://www.linkedin.com/in/chandra-prakash-pandey-6254a9223/",
-            insta:"https://www.instagram.com/chandra_p.pandey/",
-            gmail:"chandraprakashpandey2003@gmail.com"
-        },
-        {
-            id:102,
-            name:"Prajjwal Chauhan",
-            img:"",
-            git:"",
-            linkdin:"https://www.linkedin.com/in/prajjwal-chauhan-b326b120b/",
-            insta:"https://www.instagram.com/prajjwal740/",
-            gmail:""
-        },
-        ]
-    },
-    {
-    id:2,
-    name:"Dev Team",
-    members:[{
-        id:201,
-        name:"Yashpreet Singh",
-        img:yashi,
-        git:"https://github.com/s4bbi",
-        linkdin:"https://www.linkedin.com/in/s4bbi",
-        insta:"",
-        gmail:"singhyashpreet22@gmail.com"
-    },
-    {
-        id:202,
-        name:"Priya Kandpal",
-        img:priya,
-        git:"https://github.com/priya4596",
-        linkdin:"https://www.linkedin.com/in/priya-kandpal-8811ab268",
-        insta:"",
-        gmail:"priyakandpal79@gmail.com"
-    },
-    {
-        id:203,
-        name:"Nitesh Singh Bisht",
-        img:Nitesh,
-        git:"https://github.com/Niteshsinghbisht03",
-        linkdin:"https://www.linkedin.com/in/nitesh-singh-bisht",
-        insta:"",
-        gmail:"niteshsinghbisht6398@gmail.com"
-    },
-    {
-        id:204,
-        name:"Ankur Bahuguna",
-        img:ankur,
-        git:"https://github.com/bahugunaAnkur810",
-        linkdin:"https://www.linkedin.com/in/ankur-bahuguna-9075b12a7",
-        insta:"",
-        gmail:""
-    },
-    {
-        id:205,
-        name:"Vaibhav Yadav",
-        img:vaibhav,
-        git:"https://github.com/vky5",
-        linkdin:"https://www.linkedin.com/in/vaibhav-y-80969b283/",
-        insta:"",
-        gmail:"vky0579@gmail.com"
-    },
-    {
-        id:206,
-        name:"Rohit Mahant",
-        img:rohit,
+  {
+    id: 1,
+    name: "Core Team",
+    members: [
+      {
+        id: 101,
+        name: "Chandra Prakash",
+        img: chandu,
+        git: "",
+        designation: "SAC President",
+        linkdin:
+          "https://www.linkedin.com/in/chandra-prakash-pandey-6254a9223/",
+        insta: "https://www.instagram.com/chandra_p.pandey/",
+        gmail: "chandraprakashpandey2003@gmail.com",
+      },
+      {
+        id: 102,
+        name: "Prajjwal Chauhan",
+        img: "",
+        git: "",
+        designation: "Cultural Head",
+        linkdin: "https://www.linkedin.com/in/prajjwal-chauhan-b326b120b/",
+        insta: "https://www.instagram.com/prajjwal740/",
+        gmail: "",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Dev Team",
+    members: [
+      {
+        id: 201,
+        name: "Yashpreet Singh",
+        img: yashi,
+        git: "https://github.com/s4bbi",
+        linkdin: "https://www.linkedin.com/in/s4bbi",
+        insta: "",
+        gmail: "singhyashpreet22@gmail.com",
+      },
+      {
+        id: 202,
+        name: "Priya Kandpal",
+        img: priya,
+        git: "https://github.com/priya4596",
+        linkdin: "https://www.linkedin.com/in/priya-kandpal-8811ab268",
+        insta: "",
+        gmail: "priyakandpal79@gmail.com",
+      },
+      {
+        id: 203,
+        name: "Nitesh Singh Bisht",
+        img: Nitesh,
+        git: "https://github.com/Niteshsinghbisht03",
+        linkdin: "https://www.linkedin.com/in/nitesh-singh-bisht",
+        insta: "",
+        gmail: "niteshsinghbisht6398@gmail.com",
+      },
+      {
+        id: 204,
+        name: "Ankur Bahuguna",
+        img: ankur,
+        git: "https://github.com/bahugunaAnkur810",
+        linkdin: "https://www.linkedin.com/in/ankur-bahuguna-9075b12a7",
+        insta: "",
+        gmail: "",
+      },
+      {
+        id: 205,
+        name: "Vaibhav Yadav",
+        img: vaibhav,
+        git: "https://github.com/vky5",
+        linkdin: "https://www.linkedin.com/in/vaibhav-y-80969b283/",
+        insta: "",
+        gmail: "vky0579@gmail.com",
+      },
+      {
+        id: 206,
+        name: "Rohit Mahant",
+        img: rohit,
         git: "https://github.com/Semicile17",
-        linkdin:"https://www.linkedin.com/in/rohit-mahant",
-        insta:"",
-        gmail:"rohitsinghmahant707@gmail.com"
-    },]
-},
-{
-    id:3,
-    name:"Graphics Team",
-    members:[{
-        id:301,
-        name:"Niharika Chauhan",
-        img:niharika,
-        git:"",
-        linkdin:"https://www.linkedin.com/in/niharika-chauhan-351162227",
-        insta:"",
-        gmail:"niharikachauhan425@gmail.com"
-    },
-    {
-        id:302,
-        name:"Aditya Rawat",
-        img:aditya,
-        git:"",
-        linkdin:"https://www.linkedin.com/in/aditya-rawat-3a2942276",
-        insta:"",
-        gmail:""
-    },
-]
-}]
+        linkdin: "https://www.linkedin.com/in/rohit-mahant",
+        insta: "",
+        gmail: "rohitsinghmahant707@gmail.com",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Graphics Team",
+    members: [
+      {
+        id: 301,
+        name: "Niharika Chauhan",
+        img: niharika,
+        git: "",
+        linkdin: "https://www.linkedin.com/in/niharika-chauhan-351162227",
+        insta: "",
+        gmail: "niharikachauhan425@gmail.com",
+      },
+      {
+        id: 302,
+        name: "Aditya Rawat",
+        img: aditya,
+        git: "",
+        linkdin: "https://www.linkedin.com/in/aditya-rawat-3a2942276",
+        insta: "",
+        gmail: "",
+      },
+    ],
+  },
+];
