4758555c71f83814ab1f6a36ef1184372cec9748
Yashpreet Singh <singhyashpreet22@gmail.com>
2024-04-25 14:31:51 +0530

    tos update

--- frontend/src/Pages/ToS.jsx
+++ frontend/src/Pages/ToS.jsx
@@ -4,17 +4,17 @@ const ToS = () => {
   return (
     <div className='bg-EventBG pt-28 pb-10'>
         <div className='text-white w-10/12 mx-auto'>
-            <p className='text-2xl md:text-3xl font-cSB text-[#5f43b2] mb-6'>Terms and Conditions</p>
-            <p className='text-base mt-2'>
-                Effective Date: June 2024
+            <p className='text-2xl md:text-3xl font-cSB  mb-6'>Terms and Conditions</p>
+            <p className='text-base mt-2 font-cR'>Effective Date: June 2024</p>
+            <p className='text-base mt-2 font-cR'>
 
-                Welcome to goonj.gbpiet.in. These terms and conditions govern your use of the Website and any services provided therein. By accessing or using the Website, you agree to be bound by these Terms. If you do not agree to these Terms, please do not use the Website.
+                Welcome to <span className='text-[#5f43b2]'>goonj.gbpiet.in</span>, These terms and conditions govern your use of the Website and any services provided therein. By accessing or using the Website, you agree to be bound by these Terms. If you do not agree to these Terms, please do not use the Website.
             </p>
 
             <div className='flex flex-col gap-4 text-white my-6 font-cR text-sm'>
 
-                <p>
-                    <span className='font-bold text-base'>1. Use of the Website:</span>
+                <p className='font-cR'>
+                    <span className='font-bold text-lg'>1. Use of the Website:</span>
                     <br />
                     
                     Access: You may access and use the Website for lawful purposes only.
@@ -27,7 +27,7 @@ const ToS = () => {
                 </p> 
 
                 <p>
-                    <span className='font-bold text-base'>2. Payment Services </span>
+                    <span className='font-bold text-lg'>2. Payment Services </span>
                     <br />
                     
 
@@ -37,7 +37,7 @@ const ToS = () => {
 
                 <p>
 
-                    <span className='font-bold  text-base'>3. Intellectual Property</span>
+                    <span className='font-bold text-lg'>3. Intellectual Property</span>
                     <br />
                     
                     
@@ -47,7 +47,7 @@ const ToS = () => {
                 </p>
                 
                 <p> 
-                <span className='font-bold  text-base]'>4. Limitation of Liability</span>
+                <span className='font-bold text-lg'>4. Limitation of Liability</span>
                     <br />
                     
 
@@ -56,14 +56,14 @@ const ToS = () => {
                 </p>
                 
                 <p>
-                    <span className='font-bold  text-base]'>5. Identification </span>
+                    <span className='font-bold text-lg'>5. Identification </span>
                     <br />
                     
                     You agree to indemnify and hold harmless [Your Company Name], its officers, directors, employees, and agents, from and against any claims, liabilities, damages, losses, and expenses, including reasonable attorneys' fees, arising out of or in any way connected with your use of the Website or any violation of these Terms.
                 </p>
 
                 <p>
-                    <span className='font-bold  text-base]'>6. Governing Law</span>
+                    <span className='font-bold text-lg'>6. Governing Law</span>
                     <br />
                     
 
@@ -71,7 +71,7 @@ const ToS = () => {
                 </p>
 
                 <p>
-                    <span className='font-bold  text-base]'>7. Changes to terms </span>
+                    <span className='font-bold text-lg'>7. Changes to terms </span>
                     <br />
                     
 
@@ -79,7 +79,7 @@ const ToS = () => {
                 </p>
 
                 <p>
-                    <span className='font-bold  text-base]'>8. Contact Us</span>
+                    <span className='font-bold text-lg'>8. Contact Us</span>
                     <br />
                     
 
@@ -89,12 +89,12 @@ const ToS = () => {
         </div>
 
         <div className='text-white w-10/12 mx-auto'>
-            <p className='text-2xl md:text-3xl font-cSB text-[#5f43b2] mb-6'>Privacy and Policy</p>    
+            <p className='text-2xl md:text-3xl font-cSB text-[#5f43b2] mb-6'>Privacy Policy</p>    
 
             <div className='flex flex-col gap-4 text-white my-6 font-cR text-sm'>
 
                 <p>
-                    <span className='font-bold '>1. Introduction:</span>
+                    <span className='font-bold text-lg'>1. Introduction:</span>
                     <br />
                     
                     At Goonj.gbpiet.in, we are committed to protecting the privacy and security of our users' personal information. This privacy policy ("Policy") explains how we collect, use, and protect your personal information when you use our website to make payments using Razorpay.
@@ -102,7 +102,7 @@ const ToS = () => {
                 </p> 
 
                 <p>
-                    <span className='font-bold  text-base]'>2. Information we collect </span>
+                    <span className='font-bold text-lg'>2. Information we collect </span>
                     <br />
                     
 
@@ -118,7 +118,7 @@ const ToS = () => {
 
                 <p>
 
-                    <span className='font-bold  text-base]'>3. How We Use Your Information</span>
+                    <span className='font-bold text-lg'>3. How We Use Your Information</span>
                     <br />
                     We use your personal information for the following purposes:
 
@@ -131,13 +131,13 @@ const ToS = () => {
                 </p>
                 
                 <p> 
-                <span className='font-bold  text-base]'>4. How We Protect Your Information: </span>
+                <span className='font-bold text-lg'>4. How We Protect Your Information: </span>
                     <br />
                     We take reasonable measures to protect your personal information from unauthorized access, disclosure, alteration, or destruction. We use industry-standard security measures, including encryption and firewalls, to protect your personal information. However, no method of transmission over the internet, or method of electronic storage, is 100% secure. Therefore, we cannot guarantee its absolute security.
                 </p>
                 
                 <p>
-                    <span className='font-bold  text-base]'>5. Disclosure of Your Information </span>
+                    <span className='font-bold text-lg'>5. Disclosure of Your Information </span>
                     <br />
                     
                     We may disclose your personal information to the following parties:
@@ -148,7 +148,7 @@ const ToS = () => {
                 </p>
 
                 <p>
-                    <span className='font-bold  text-base]'>6. Your Rights</span>
+                    <span className='font-bold text-lg'>6. Your Rights</span>
                     <br />
 
                     You have certain rights with respect to your personal information, including:
@@ -162,7 +162,7 @@ const ToS = () => {
                 </p>
 
                 <p>
-                    <span className='font-bold  text-base]'>7. Contact Us</span>
+                    <span className='font-bold text-lg'>7. Contact Us</span>
                     <br />
                     
 
@@ -170,7 +170,7 @@ const ToS = () => {
                 </p>
 
                 <p>
-                    <span className='font-bold  text-base]'>8. Changes to This Policy</span>
+                    <span className='font-bold text-lg'>8. Changes to This Policy</span>
                     <br />
                     
 
@@ -179,7 +179,7 @@ const ToS = () => {
                 </p>
 
                 <p>
-                    <span className='font-bold  text-base]'>9. Governing Law: </span>
+                    <span className='font-bold text-lg'>9. Governing Law: </span>
                     <br />
                     
                     This Policy shall be governed by and construed in accordance with the laws of India. Any disputes arising out of or in connection with this Policy shall be subject to the exclusive jurisdiction of the courts of Pauri, Garhwal.
@@ -187,7 +187,7 @@ const ToS = () => {
                 </p>
 
                 <p>
-                    <span className='font-bold  text-base]'>9. Effective Date: </span>
+                    <span className='font-bold text-lg'>9. Effective Date: </span>
                     <br />
                     
                     This Policy is effective as of June 2024.
