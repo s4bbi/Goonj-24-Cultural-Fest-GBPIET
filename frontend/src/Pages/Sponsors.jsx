dead922e41c0cf7865c1f2705c84ba69508d9002
Yashpreet Singh <singhyashpreet22@gmail.com>
2024-05-04 01:35:41 +0530

    Sponsors Page Update

--- frontend/src/Pages/Sponsors.jsx
+++ frontend/src/Pages/Sponsors.jsx
@@ -4,6 +4,7 @@ import telescope2 from "../assets/Images/telescope2.png";
 import SBI_Logo from "../assets/Images/SBI_Logo.svg"
 import LIC_Logo from "../assets/Images/LIC_Logo.png"
 import KwaliFoods_Logo from "../assets/Images/KwaliFoods_Logo.png"
+import { Link } from "react-router-dom";
 
 const Sponsors = () => {
     return (
@@ -33,7 +34,7 @@ const Sponsors = () => {
                 </div>
                 <div className="md:w-8/12 flex flex-col md:flex-row px-10 gap-20 justify-between items-center mx-auto">
                     <img src={telescope} className="hidden md:flex w-56" alt="" />
-                    <img src={KwaliFoods_Logo} className="w-24 md:w-48" alt="" />
+                    <Link to="https://www.instagram.com/kwalifoods?igsh=MTVpa3RxNGk5aGg1aw=="><img src={KwaliFoods_Logo} className="w-24 md:w-48" alt="" /></Link>
                 </div>
             </div>
             <div>
