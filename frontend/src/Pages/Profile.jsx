54a5451076d0754c640fff374267051f63c09922
Rohit Mahant <138305306+Semicile17@users.noreply.github.com>
2024-05-08 23:02:59 +0530

    Update Profile.jsx

--- frontend/src/Pages/Profile.jsx
+++ frontend/src/Pages/Profile.jsx
@@ -67,7 +67,7 @@ const Profile = () => {
         <span className="font-medium text-lg border-b-2 border-[#5f43b2]">
           REGISTERED EVENTS
         </span>
-       <div className="max-h-10 overflow-y-auto scrollbar-hide">
+       <div className="max-h-10 overflow-y-auto">
         <ul>
           {Array.isArray(receivedUserData.events) && receivedUserData.events.map((event) => (
             <li key={event} className="p-2" style={{ display: 'inline-block', margin: '0 5px'}}>
