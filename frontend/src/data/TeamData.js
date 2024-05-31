159624405755f643f88aca3777912252e639ec3f
Nitesh Singh Bisht <niteshsinghbisht003@gmail.com>
2024-05-31 21:26:25 +0530

    minor update

--- frontend/src/data/TeamData.js
+++ frontend/src/data/TeamData.js
@@ -53,7 +53,7 @@ import kapil from "../assets/Images/TeamsImages/EventTeam/kapil.jpg";
 import asmita from "../assets/Images/TeamsImages/EventTeam/asmita.jpg";
 import ujjwal from "../assets/Images/TeamsImages/EventTeam/ujjwal.jpeg";
 import jugbir from "../assets/Images/TeamsImages/EventTeam/jugbir.jpg";
-import drona from "../assets/Images/TeamsImages/EventTeam/drona.PNG";
+import drona from "../assets/Images/TeamsImages/EventTeam/drona.jpg";
 import ashi from "../assets/Images/TeamsImages/EventTeam/ashlesh.jpg";
 export const teamData = [
   {
@@ -337,7 +337,27 @@ export const teamData = [
         gmail: "kritiuniyal28@gmail.com",
         designation: "Event Coordinator",
       },
-    
+      {
+        id: 2012,
+        name: "Drona Chamoli",
+        img: drona,
+        git: "",
+        linkdin: "",
+        insta:
+          "https://www.instagram.com/drona_07?igsh=MWU1c3Vka3p1c2F4aQ%3D%3D&utm_source=qr",
+        gmail: "",
+        designation: "Event Coordinator",
+      },
+      {
+        id: 2102,
+        name: "Ashlesh Ranjan",
+        img: ashi,
+        git: "",
+        linkdin: "",
+        insta: "https://www.instagram.com/ashlesh_ranjan?igsh=cmMwbnc4N3E1Z3My",
+        gmail: "",
+        designation: "Event Coordinator",
+      },
       {
         id: 213,
         name: "Rashmi Rawat",
